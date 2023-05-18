bb0ef497263381ced10ba15124aacd80bb19aeb0
ilfat hafizov <ilfat.hafizov2@yandex.ru>
2023-05-18 17:25:34 +0300

    update main and add docker compose postgresql

--- tracker-dao/src/main/java/org/example/tracker/dao/entity/TeamInfoEntity.java
+++
@@ -1,9 +0,0 @@
-package org.example.tracker.dao.entity;
-
-import org.example.tracker.dao.enums.EmployeeRole;
-
-public class TeamInfoEntity {
-    private int projectCode;
-    private EmployeeRole role;
-    private int maxRoleSize;
-}
