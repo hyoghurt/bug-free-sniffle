7b1fc535bd2c9173acc1ece803e7e415046e3c1b
ilfat hafizov <ilfat.hafizov2@yandex.ru>
2023-05-29 21:00:08 +0300

    create jpaConfig file

--- tracker-main/src/main/java/org/example/tracker/TrackerApplication.java
+++ tracker-main/src/main/java/org/example/tracker/TrackerApplication.java
@@ -2,12 +2,8 @@ package org.example.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "org.example.tracker")
-@EnableJpaRepositories(basePackages = "org.example.tracker.dao")
-@EntityScan("org.example.tracker.dao")
 public class TrackerApplication {
 
 	public static void main(String[] args) {
